ec06efed593288d13f80e84437de71f99ecb6c26
Mwakai <mwakaimwambala@gmail.com>
2024-07-04 18:55:28 +0300

    feat: setup the database and registration of users

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const sequelize = require("./config/database");
+const User = require("./models/User");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync();
+
+app.post("api/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(200).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error("Error registering user", error);
+    res.status(500).json({
+      error: "Error registering user",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
