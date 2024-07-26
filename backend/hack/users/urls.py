d7370920e32da346662f82219feb3c06dd7f8556
Mwakai <mwakaimwambala@gmail.com>
2024-07-26 13:15:42 +0300

    remove py

--- backend/hack/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import Register, LogIn
-
-urlpatterns = [
-    path("register/", Register.as_view()),
-    path("login/", LogIn.as_view()),
-    path("my-profile/", include("profiles.urls")),
-    path("<str:user_id>/allergies/", include("allergy.urls"))
-]
