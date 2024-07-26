949280aaa8436560dc2bbf0e9e133f207fb56361
Mwakai <mwakaimwambala@gmail.com>
2024-07-26 13:13:31 +0300

    Update backend contents from node-backend branch

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/database");
+
+const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  height: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  weight: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  allergies: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = User;
